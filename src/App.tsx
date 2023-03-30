04d0e9ce12b1def6a861b6b2fcb0459148b21be0
hyeonseok <tobble93@gmail.com>
2023-03-30 19:22:31 +0900

    eslint 셋팅

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,7 @@
-const App = () => {
-  return <div>없어 이건 아무것도 없어</div>;
+import React from "react";
+
+const App = (): JSX.Element => {
+  return <div>없어 이건 아무것도 없어, 히히히 헤헤</div>;
 };
 
 export default App;
