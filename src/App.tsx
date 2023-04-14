e829ec8c1367d025d2ccb0a8828fdcb0450283bd
hyeonseok <tobble93@gmail.com>
2023-04-14 11:14:28 +0900

    RTF

--- src/App.tsx
+++ src/App.tsx
@@ -4,12 +4,14 @@ import GlobalStyle from "./styles/globalStyle";
 import { lightTheme, darkTheme } from "./styles/theme";
 import Layout from "./pages/Layout";
 import Routers from "./routers";
+import ReactThreeFiber from "./pages/ReactThreeFiber";
 
 const App = (): JSX.Element => {
   return (
-    <Layout>
-      <Routers />
-    </Layout>
+    // <Layout>
+    //   <Routers />
+    // </Layout>
+    <ReactThreeFiber />
   );
 };
 
---
+++ src/pages/ReactThreeFiber/index.tsx
@@ -0,0 +1,7 @@
+import React from "react";
+
+const ReactThreeFiber = (): JSX.Element => {
+  return <div>R-T-F</div>;
+};
+
+export default ReactThreeFiber;
