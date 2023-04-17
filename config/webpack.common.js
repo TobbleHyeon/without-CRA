3262bb28930195733335488e3c95674fd20825d3
hyeonseok <tobble93@gmail.com>
2023-04-17 17:57:07 +0900

    리프레시 관련 로더 추가

--- config/webpack.common.js
+++ config/webpack.common.js
@@ -1,57 +1,70 @@
-const HtmlWebpackPlugin = require('html-webpack-plugin');
-const path = require('path');
-const webpack = require('webpack');
-const { CleanWebpackPlugin } = require('clean-webpack-plugin');
+const HtmlWebpackPlugin = require("html-webpack-plugin");
+const path = require("path");
+const webpack = require("webpack");
+const { CleanWebpackPlugin } = require("clean-webpack-plugin");
+const RefreshWebpackPlugin = require("@pmmmwh/react-refresh-webpack-plugin");
 
 // const { BundleAnalyzerPlugin } = require("webpack-bundle-analyzer");
 // BundleAnalyzer는 Bundle 최적화 용도.
 
 module.exports = {
-  entry: `${path.resolve(__dirname, '../src')}/index.tsx`,
+  entry: `${path.resolve(__dirname, "../src")}/index.tsx`,
   module: {
     rules: [
       {
         test: /\.(ts|tsx|js|jsx)$/,
-        use: 'babel-loader',
         exclude: /node_modules/,
+        use: {
+          loader: "babel-loader",
+          options: {
+            plugins: ["react-refresh/babel"],
+          },
+        },
       },
       {
         test: /\.(?:ico|gif|png|jpg|jpeg)$/i,
-        type: 'asset/resource',
+        type: "asset/resource",
       },
       {
         test: /\.(woff(2)?|eot|ttf|otf)$/,
-        type: 'asset/resource',
+        type: "asset/resource",
       },
       {
         test: /\.svg$/,
         use: [
-          '@svgr/webpack',
+          "@svgr/webpack",
           {
-            loader: 'url-loader',
+            loader: "url-loader",
             options: {
               limit: 10240, // 10KB 이하인 svg인 경우, url-loader를 사용. limit을 지정하지 않을 경우, 8kb로 고정.
-              name: 'images/[name].[ext]?[hash]',
-              fallback: 'file-loader' // 10KB 이상인 경우, url-loader 대신 file-loader 사용.
+              name: "images/[name].[ext]?[hash]",
+              fallback: "file-loader", // 10KB 이상인 경우, url-loader 대신 file-loader 사용.
             },
           },
         ],
+      },
+      // favicion
+      {
+        test: /\.ico$/,
+        exclude: /node_modules/,
+        use: ["file-loader?name=[name].[ext]"],
       },
     ],
   },
   plugins: [
     new HtmlWebpackPlugin({
-      template: 'public/index.html',
+      template: "public/index.html",
     }),
     new webpack.ProvidePlugin({
-      React: 'react',
+      React: "react",
     }),
     new CleanWebpackPlugin({}),
+    new RefreshWebpackPlugin({}),
   ],
   resolve: {
     alias: {
-      '@': path.resolve(__dirname, '../src/'),
+      "@": path.resolve(__dirname, "../src/"),
     },
-    extensions: ['.js', '.ts', '.jsx', '.tsx', '.css', '.scss', '.json'],
+    extensions: [".js", ".ts", ".jsx", ".tsx", ".css", ".scss", ".json"],
   },
 };
--- config/webpack.dev.js
+++ config/webpack.dev.js
@@ -1,26 +1,31 @@
-const { merge } = require('webpack-merge');
-const common = require('./webpack.common');
+const { merge } = require("webpack-merge");
+const common = require("./webpack.common");
 
 module.exports = merge(common, {
-  mode: 'development',
-  devtool: 'inline-source-map',
+  mode: "development",
+  devtool: "inline-source-map",
   devServer: {
     open: false, // dev server 구동 후 브라우저 열기
     hot: true, // webpack의 HMR 기능 활성화
     compress: true, // 모든 항목에 대해 gzip압축 사용
     port: 3005, // server port 설정
-    historyApiFallback: true,
+    historyApiFallback: true, // 404 에러 발생 시, index.html로 이동.
     liveReload: true, // 파일 변경이 감지되면 페이지를 다시 로드하고 새로고침
+    client: {
+      progress: false, // 브라우저에서 컴파일 진행률을 백분율로 출력함.
+      overlay: true, // 컴파일러 오류나 경고를 브라우저 화면에 오버레이를 표시함.
+      logging: "error", // 브라우저 로그 수준을 설정 'log' | 'info' | 'warn' | 'error' | 'none' | 'verbose'
+    },
   },
   output: {
-    filename: '[name].[contenthash].js',
-    publicPath: '/',
+    filename: "[name].[contenthash].js",
+    publicPath: "/",
   },
   module: {
     rules: [
       {
         test: /\.(sa|sc|c)ss$/i,
-        use: ['style-loader', 'css-loader', 'sass-loader'],
+        use: ["style-loader", "css-loader", "sass-loader"],
       },
     ],
   },
